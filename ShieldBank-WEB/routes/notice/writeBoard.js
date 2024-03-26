2d57cdde25794f8316ba72bf461170688e25b6d3
RyuSeung <48815923+RyuSeung@users.noreply.github.com>
2024-03-26 12:51:20 +0900

    Update writeBoard.js

--- ShieldBank-WEB/routes/notice/writeBoard.js
+++ ShieldBank-WEB/routes/notice/writeBoard.js
@@ -79,7 +79,7 @@ router.post("/write",
             // 데이터베이스에 데이터를 삽입
             db.query(
                 `INSERT INTO notices
-                 VALUES (NULL, '${userId}', '${title}', '${contents}', '${req.file ? req.file.path.replace : "null"}', '${seoultime}', '${seoultime}')`,
+                 VALUES (NULL, '${userId}', '${title}', '${contents}', '${req.file ? req.file.path : "null"}', '${seoultime}', '${seoultime}')`,
                 function (error, results) {
                     if (error) {
                         throw error;
