d31d7b33e45bdf9c1f664166f57d1b0a2f9ede3f
ryu <rhekfl221@gmail.com>
2024-04-06 00:31:39 +0900

    merge

--- Local/ShieldBank/middlewares/userdb.js
+++
@@ -1,9 +0,0 @@
-var mysql = require("mysql2");
-var userdb = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "1234",
-  database: "dvba",
-  dateStrings: "date",
-});
-module.exports = userdb;
